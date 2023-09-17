a485b0a99319a0653ef6c1dcb003d5f1789bbc16
xaer981 <xaer981@yandex.ru>
2023-09-17 13:03:06 +0300

    added default to os.getenv in settings.py

--- settings.py
+++ settings.py
@@ -1,7 +1,11 @@
 import os
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', default='sqlite:///db.sqlite3')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.getenv('SECRET_KEY')
+    SECRET_KEY = os.getenv('SECRET_KEY', default='SECRET_KEY')
